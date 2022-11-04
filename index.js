d7358db758a859f9cdfa1e78f4e527a4a90a8a28
maruf <dev.maruf112@gmail.com>
2022-11-04 18:31:52 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -12,6 +12,23 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("I am running perfectfly...");
 });
+
+// connect with mongodb
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.efpjwcu.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 // listen
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
